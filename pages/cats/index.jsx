bdda03cd5fa5780eaeed9a4718b1dbe6f3023650
Evren Ceyhan <evren@ceyhan.io>
2022-05-03 16:37:10 +0300

    cats fecthed from api

--- pages/cats/index.jsx
+++ pages/cats/index.jsx
@@ -1,3 +1,26 @@
+import { useState, useEffect } from 'react';
+import Card from '../../components/Card/Card';
+
 export default function Cats() {
-    return <div>cats page</div>;
+    const [cats, setCats] = useState([]);
+
+    const fetchCats = async () => {
+        const response = await fetch('/api/cats');
+        const cats = await response.json();
+        setCats(cats);
+    };
+
+    useEffect(() => {
+        fetchCats();
+    }, []);
+
+    return (
+        <div>
+            <h1> cats page</h1>
+
+            {cats.map((cat, index) => (
+                <Card key={index} {...cat} />
+            ))}
+        </div>
+    );
 }
