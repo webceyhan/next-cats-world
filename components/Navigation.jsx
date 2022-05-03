e34f52d632ccefbcf51defbf9749d514a1b9e6db
Evren Ceyhan <evren@ceyhan.io>
2022-05-03 16:51:33 +0300

    nav link update

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,8 +1,8 @@
 export const Navigation = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand navbar-light bg-light">
             <div className="container">
-                <a className="navbar-brand" href="#">
+                <a className="navbar-brand" href="/">
                     Navbar
                 </a>
                 <button
@@ -16,17 +16,13 @@ export const Navigation = () => {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a
-                                className="nav-link"
-                                aria-current="page"
-                                href="#"
-                            >
+                            <a className="nav-link" href="/">
                                 Home
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                Features
+                            <a className="nav-link" href="/cats">
+                                Cats
                             </a>
                         </li>
                     </ul>
